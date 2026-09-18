fe39fa0f651cd4e5965fa542cd9e1edf02adaf2a
vishalminj <vishalminj667@gmail.com>
2026-09-18 16:11:35 +0530

    frontend boilerplate setup

---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
